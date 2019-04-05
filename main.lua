febb24cd4efc647dff1a790908dcb1a1542c6f59
bridgs <bridgs.dev@gmail.com>
2019-04-05 14:57:15 -0400

    Cleaning up game

--- main.lua
+++ main.lua
@@ -1,65 +1,68 @@
--- Constants
-local GAME_WIDTH = 150
-local GAME_HEIGHT = 150
-local RENDER_SCALE = 4
+-- Render constants
+local GAME_WIDTH = 192
+local GAME_HEIGHT = 192
+local RENDER_SCALE = 3
 
--- Game objects
+-- Game variables
 local car
 local puffs
-
--- Images
-local carImage
-local raceTrackImage
 local raceTrackData
 
--- Souned effects
-local engine1Sound
-local engine2Sound
-local engine3Sound
-local engine4Sound
+-- Assets
+local carImage
+local raceTrackImage
+local engineSounds
 local crashSound
 
 -- Initializes the game
 function love.load()
-  -- Load images
-  carImage = loadImage('img/car.png')
-  raceTrackImage = loadImage('img/race-track.png')
-  raceTrackData = love.image.newImageData('img/race-track-data.png')
-
-  -- Load sound effects
-  engine1Sound = love.audio.newSource('sfx/engine1.wav', 'static')
-  engine2Sound = love.audio.newSource('sfx/engine2.wav', 'static')
-  engine3Sound = love.audio.newSource('sfx/engine3.wav', 'static')
-  engine4Sound = love.audio.newSource('sfx/engine4.wav', 'static')
+  -- Load assets
+  carImage = love.graphics.newImage('img/car.png')
+  raceTrackImage = love.graphics.newImage('img/race-track.png')
+  carImage:setFilter('nearest', 'nearest')
+  raceTrackImage:setFilter('nearest', 'nearest')
+  engineSounds = {
+    love.audio.newSource('sfx/engine1.wav', 'static'),
+    love.audio.newSource('sfx/engine2.wav', 'static'),
+    love.audio.newSource('sfx/engine3.wav', 'static'),
+    love.audio.newSource('sfx/engine4.wav', 'static')
+  }
   crashSound = love.audio.newSource('sfx/crash.wav', 'static')
 
-  -- Create the game objects
+  -- Load race track data (from an image)
+  raceTrackData = love.image.newImageData('img/race-track-data.png')
+
+  -- Create the car and an array for puffs of exhaust
   puffs = {}
-  createCar()
+  car = createCar()
 end
 
 -- Updates the game state
 function love.update(dt)
-  -- Spawn a puff of smoke every now and then
+  -- Spawn a puff of exhaust every now and then
   car.puffTimer = car.puffTimer + dt
   if car.puffTimer > 1.25 - math.abs(car.speed) / 40 then
     car.puffTimer = 0.00
-    createPuff(car.x, car.y)
+    table.insert(puffs, {
+      x = car.x,
+      y = car.y,
+      timeToDisappear = 2.00
+    })
   end
 
-  -- Make some engine noises
+  -- Make some engine noises, with a higher pitch when the car is going faster
   car.engineNoiseTimer = car.engineNoiseTimer + dt
   if car.engineNoiseTimer > 0.17 then
     car.engineNoiseTimer = 0.00
     local speed = math.abs(car.speed)
     if speed > 30 then
-      love.audio.play(engine4Sound:clone())
+      love.audio.play(engineSounds[4]:clone())
     elseif speed > 20 then
-      love.audio.play(engine3Sound:clone())
+      love.audio.play(engineSounds[3]:clone())
     elseif speed > 10 then
-      love.audio.play(engine2Sound:clone())
+      love.audio.play(engineSounds[2]:clone())
     else
-      love.audio.play(engine1Sound:clone())
+      love.audio.play(engineSounds[1]:clone())
     end
   end
 
@@ -74,7 +77,7 @@ function love.update(dt)
     car.speed = car.speed * 0.98
   end
 
-  -- Turn the car
+  -- Turn the car by pressing the left and right keys
   local turnSpeed = 3 * math.min(math.max(0, math.abs(car.speed) / 20), 1) - (car.speed > 20 and (car.speed - 20) / 20 or 0)
   if love.keyboard.isDown('left') then
     car.rotation = car.rotation - turnSpeed * dt
@@ -88,7 +91,7 @@ function love.update(dt)
   car.x = car.x + car.speed * -math.sin(car.rotation) * dt + car.bounceVelocityX * dt
   car.y = car.y + car.speed * math.cos(car.rotation) * dt + car.bounceVelocityY * dt
 
-  -- Update the puffs of smoke
+  -- Update the puffs of exhaust
   for i = #puffs, 1, -1 do
     local puff = puffs[i]
     puff.y = puff.y - 10 * dt
@@ -99,7 +102,9 @@ function love.update(dt)
   end
 
   -- Check what terrain the car is currently on by looking at the race track data image
-  local r, g, b = raceTrackData:getPixel(math.min(math.max(0, math.floor(car.x)), 149), math.min(math.max(0, math.floor(car.y)), 149))
+  local pixelX = math.min(math.max(0, math.floor(car.x)), 191)
+  local pixelY = math.min(math.max(0, math.floor(car.y)), 191)
+  local r, g, b = raceTrackData:getPixel(pixelX, pixelY)
   local isInBarrier = r > 0 -- red means barriers
   local isInRoughTerrain = b > 0 -- blue means rough terrain
 
@@ -120,22 +125,19 @@ function love.update(dt)
   car.bounceVelocityX = car.bounceVelocityX * 0.90
   car.bounceVelocityY = car.bounceVelocityY * 0.90
 
-  -- If the car ever gets out of bound, reset the car
+  -- If the car ever gets out of bounds, reset it
   if car.x < 0 or car.y < 0 or car.x > GAME_WIDTH or car.y > GAME_HEIGHT then
-    createCar()
+    car = createCar()
   end
 end
 
 -- Renders the game
 function love.draw()
-  -- Set some drawing filters
-  love.graphics.setDefaultFilter('nearest', 'nearest')
+  -- Scale and crop the screen
+  love.graphics.setScissor(0, 0, RENDER_SCALE * GAME_WIDTH, RENDER_SCALE * GAME_HEIGHT)
   love.graphics.scale(RENDER_SCALE, RENDER_SCALE)
-
-  -- Clear the screen
-  love.graphics.setColor(0, 0, 0, 1)
-  love.graphics.rectangle('fill', 0, 0, GAME_WIDTH, GAME_HEIGHT)
-  love.graphics.setColor(1, 1, 1, 1)
+  love.graphics.clear(0, 0, 0)
+  love.graphics.setColor(1, 1, 1)
 
   -- Draw the race track
   love.graphics.draw(raceTrackImage, 0, 0)
@@ -146,10 +148,10 @@ function love.draw()
   if car.rotation >= math.pi then
     sprite = 18 - sprite
   end
-  drawSprite(carImage, sprite, 12, 12, car.rotation >= math.pi, car.x - 6, car.y - 6)
+  drawSprite(carImage, 12, 12, sprite, car.x - 6, car.y - 6, car.rotation >= math.pi)
 
-  -- Draw the puffs of smoke coming out of the car
-  love.graphics.setColor(115 / 255, 105 / 255, 92 / 255, 1)
+  -- Draw the puffs of exhaust coming out of the car
+  love.graphics.setColor(142 / 255, 92 / 255, 111 / 255)
   for _, puff in ipairs(puffs) do
     love.graphics.rectangle('fill', puff.x - 1, puff.y - 1, 2, 2)
   end
@@ -157,7 +159,7 @@ end
 
 -- Creates the playable car
 function createCar()
-  car = {
+  return {
     x = 95,
     y = 28,
     bounceVelocityX = 0,
@@ -169,27 +171,15 @@ function createCar()
   }
 end
 
--- Creates a puff of smoke
-function createPuff(x, y)
-  table.insert(puffs, {
-    x = x,
-    y = y,
-    timeToDisappear = 2.00
-  })
-end
-
--- Loads a pixelated image
-function loadImage(filePath)
-  local image = love.graphics.newImage(filePath)
-  image:setFilter('nearest', 'nearest')
-  return image
-end
-
--- Draws a sprite from a sprite sheet image, spriteNum=1 is the upper-leftmost sprite
-function drawSprite(image, spriteNum, spriteWidth, spriteHeight, flipHorizontally, x, y)
-  local columns = math.floor(image:getWidth() / spriteWidth)
-  local col = (spriteNum - 1) % columns
-  local row = math.floor((spriteNum - 1) / columns)
-  local quad = love.graphics.newQuad(spriteWidth * col, spriteHeight * row, spriteWidth, spriteHeight, image:getDimensions())
-  love.graphics.draw(image, quad, x + (flipHorizontally and spriteWidth or 0), y, 0, flipHorizontally and -1 or 1, 1)
+-- Draws a sprite from a sprite sheet, spriteNum=1 is the upper-leftmost sprite
+function drawSprite(spriteSheetImage, spriteWidth, spriteHeight, sprite, x, y, flipHorizontal, flipVertical, rotation)
+  local width, height = spriteSheetImage:getDimensions()
+  local numColumns = math.floor(width / spriteWidth)
+  local col, row = (sprite - 1) % numColumns, math.floor((sprite - 1) / numColumns)
+  love.graphics.draw(spriteSheetImage,
+    love.graphics.newQuad(spriteWidth * col, spriteHeight * row, spriteWidth, spriteHeight, width, height),
+    x + spriteWidth / 2, y + spriteHeight / 2,
+    rotation or 0,
+    flipHorizontal and -1 or 1, flipVertical and -1 or 1,
+    spriteWidth / 2, spriteHeight / 2)
 end
